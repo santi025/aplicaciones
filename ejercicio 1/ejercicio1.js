69c8df3839dc666e94120484130750fb2196ee16
santiO25 <aslancheros@uniboyaca.edu.co>
2024-04-18 17:20:48 -0500

    agreglos .2

--- ejercicio 1/ejercicio1.js
+++ ejercicio 1/ejercicio1.js
@@ -2,22 +2,25 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
     event.preventDefault();
     
     const patientName = document.getElementById('patientName').value;
+    const patientApellido = document.getElementById('patientApellido').value;
     const appointmentDate = document.getElementById('appointmentDate').value;
     const doctor = document.getElementById('doctor').value;
   
     const appointment = {
       patientName: patientName,
+      patientApellido:patientApellido,
       appointmentDate: appointmentDate,
       doctor: doctor
     };
   
     const listItem = document.createElement('li');
-    listItem.textContent = `${appointment.patientName} - ${appointment.appointmentDate} - ${appointment.doctor}`;
+    listItem.textContent = `${appointment.patientName}-${appointment.patientApellido} - ${appointment.appointmentDate} - ${appointment.doctor}`;
   
     document.getElementById('appointmentList').appendChild(listItem);
   
     
     document.getElementById('patientName').value = '';
+    document.getElementById('patientApellido').value = '';
     document.getElementById('appointmentDate').value = '';
     document.getElementById('doctor').value = '';
   });
